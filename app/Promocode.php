420fcbc0c90adf2b082970587ec66afc4d70b17d
Fashola Ayodeji F <fashtop3@gmail.com>
2016-09-26 18:56:40 +0000

    promo update

---
+++ app/Promocode.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Promocode extends Model
+{
+    //
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'code',
+        'reward',
+        'quantity',
+        'is_used',
+    ];
+}
