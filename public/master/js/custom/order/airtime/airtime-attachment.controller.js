66665178997958915ec9c63aea338170f37c8801
Fashola Ayodeji <fashtop3@gmail.com>
2016-10-21 10:02:57 +0100

    run bower install and npm install

---
+++ public/master/js/custom/order/airtime/airtime-attachment.controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dfash on 10/21/16.
+ */
+
+(function() {
+
+    angular
+        .module('app.order')
+        .controller('AirtimeAttachment', ['$scope', function($scope) {
+
+        }]);
+
+})();
