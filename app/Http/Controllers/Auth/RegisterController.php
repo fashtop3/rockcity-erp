06d61d5d5d618673d1409c48c62e275f9027d67a
Fashola Ayodeji <fashtop3@gmail.com>
2017-04-25 20:46:38 +0100

    added mail todo to RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,8 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+
+        //Todo: handle registration mail
         return User::create([
             'firstname' => $data['firstname'],
             'lastname' => $data['lastname'],
